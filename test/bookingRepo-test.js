a2a0a95165b7f1722172530ba7bfca64fbce64ba
Janika Hortizuela <jhortizuela21@gmail.com>
2021-09-28 10:20:50 -0600

    Refactor test tiles

--- test/bookingRepo-test.js
+++ test/bookingRepo-test.js
@@ -3,13 +3,10 @@ import BookingRepo from '../src/classes/bookingRepo.js'
 const expect = chai.expect;
 
 describe('Bookings', () => {
+  let rooms, booking
 
-  it('should be a function', () => {
-    expect(BookingRepo).to.be.a('function')
-  });
-
-  it('should display only rooms that are associated with the customerID', () => {
-    let rooms = [{
+  beforeEach(() => {
+    rooms = [{
       "number": 1,
       "roomType": "residential suite",
       "bidet": true,
@@ -62,7 +59,7 @@ describe('Bookings', () => {
       [{
           "id": "5fwrgu4i7k55hl6sz",
           "userID": 21,
-          "date": "2020/0/16",
+          "date": "2020/01/16",
           "roomNumber": 1,
           "roomServiceCharges": []
       },
@@ -87,11 +84,18 @@ describe('Bookings', () => {
           "roomNumber": 4,
           "roomServiceCharges": []
       }])
-      expect(booking.getOnlyCustomer(21, rooms)).to.deep.equal([
+  });
+
+  it('should be a function', () => {
+    expect(BookingRepo).to.be.a('function')
+  });
+
+  it('should display only rooms that are associated with the customerID', () => {
+    expect(booking.getOnlyCustomer(21, rooms)).to.deep.equal([
         {
           "id": "5fwrgu4i7k55hl6sz",
           "userID": 21,
-          "date": "2020/0/16",
+          "date": "2020/01/16",
           "roomNumber": 1,
           "roomServiceCharges": [],
           "roomType": "residential suite",
@@ -111,188 +115,30 @@ describe('Bookings', () => {
           "bedSize": "king",
           "numBeds": 1,
           "costPerNight": 491.14
-      },
-    ])
-  })
+        }
+      ])
+    })
 
   it('should return customer past bookings', () => { 
-    let rooms = [{
-      "number": 1,
-      "roomType": "residential suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 358.4
-  },
-  {
-      "number": 2,
-      "roomType": "suite",
-      "bidet": false,
-      "bedSize": "full",
-      "numBeds": 2,
-      "costPerNight": 477.38
-  },
-  {
-      "number": 3,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "king",
-      "numBeds": 1,
-      "costPerNight": 491.14
-  },
-  {
-      "number": 4,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 429.44
-  },
-  {
-      "number": 5,
-      "roomType": "single room",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 2,
-      "costPerNight": 340.17
-  },
-  {
-      "number": 6,
-      "roomType": "junior suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 397.02
-  }],
-
-    booking = new BookingRepo(
-      [{
-          "id": "5fwrgu4i7k55hl6sz",
-          "userID": 21,
-          "date": "2020/01/16",
-          "roomNumber": 1,
-          "roomServiceCharges": []
-      },
+    booking.getOnlyCustomer(21, rooms)
+    expect(booking.getPastBookings("2020/02/01")).to.deep.equal([
       {
-          "id": "5fwrgu4i7k55hl6t5",
-          "userID": 43,
-          "date": "2020/02/16",
-          "roomNumber": 2,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t6",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 3,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t7",
-          "userID": 20,
-          "date": "2020/02/16",
-          "roomNumber": 4,
-          "roomServiceCharges": []
-      }])
-      
-      booking.getOnlyCustomer(21, rooms)
-      expect(booking.getPastBookings("2020/02/01")).to.deep.equal([
-        {
-          "id": "5fwrgu4i7k55hl6sz",
-          "userID": 21,
-          "date": "2020/01/16",
-          "roomNumber": 1,
-          "roomServiceCharges": [],
-          "roomType": "residential suite",
-          "bidet": true,
-          "bedSize": "queen",
-          "numBeds": 1,
-          "costPerNight": 358.4
-        }
+        "id": "5fwrgu4i7k55hl6sz",
+        "userID": 21,
+        "date": "2020/01/16",
+        "roomNumber": 1,
+        "roomServiceCharges": [],
+        "roomType": "residential suite",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 358.4
+      }
     ])
   })
 
   it('should return customer current bookings', () => { 
-    let rooms = [{
-      "number": 1,
-      "roomType": "residential suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 358.4
-  },
-  {
-      "number": 2,
-      "roomType": "suite",
-      "bidet": false,
-      "bedSize": "full",
-      "numBeds": 2,
-      "costPerNight": 477.38
-  },
-  {
-      "number": 3,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "king",
-      "numBeds": 1,
-      "costPerNight": 491.14
-  },
-  {
-      "number": 4,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 429.44
-  },
-  {
-      "number": 5,
-      "roomType": "single room",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 2,
-      "costPerNight": 340.17
-  },
-  {
-      "number": 6,
-      "roomType": "junior suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 397.02
-  }],
-
-    booking = new BookingRepo(
-      [{
-          "id": "5fwrgu4i7k55hl6sz",
-          "userID": 21,
-          "date": "2020/01/16",
-          "roomNumber": 1,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t5",
-          "userID": 43,
-          "date": "2020/02/16",
-          "roomNumber": 2,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t6",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 3,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t7",
-          "userID": 20,
-          "date": "2020/02/16",
-          "roomNumber": 4,
-          "roomServiceCharges": []
-      }])
-      
-      booking.getOnlyCustomer(21, rooms)
+     booking.getOnlyCustomer(21, rooms)
       expect(booking.getCurrentBookings("2020/01/16")).to.deep.equal([
         {
           "id": "5fwrgu4i7k55hl6sz",
@@ -310,444 +156,66 @@ describe('Bookings', () => {
   })
 
   it('should return customer future bookings', () => { 
-    let rooms = [{
-      "number": 1,
-      "roomType": "residential suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 358.4
-  },
-  {
-      "number": 2,
-      "roomType": "suite",
-      "bidet": false,
-      "bedSize": "full",
-      "numBeds": 2,
-      "costPerNight": 477.38
-  },
-  {
-      "number": 3,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "king",
-      "numBeds": 1,
-      "costPerNight": 491.14
-  },
-  {
-      "number": 4,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 429.44
-  },
-  {
-      "number": 5,
-      "roomType": "single room",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 2,
-      "costPerNight": 340.17
-  },
-  {
-      "number": 6,
-      "roomType": "junior suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 397.02
-  }],
-
-    booking = new BookingRepo(
-      [{
-          "id": "5fwrgu4i7k55hl6sz",
-          "userID": 21,
-          "date": "2020/01/16",
-          "roomNumber": 1,
-          "roomServiceCharges": []
-      },
+    booking.getOnlyCustomer(21, rooms)
+    expect(booking.getFutureBookings("2020/01/16")).to.deep.equal([
       {
-          "id": "5fwrgu4i7k55hl6t5",
-          "userID": 43,
-          "date": "2020/02/16",
-          "roomNumber": 2,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t6",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 3,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t7",
-          "userID": 20,
-          "date": "2020/02/16",
-          "roomNumber": 4,
-          "roomServiceCharges": []
-      }])
-      
-      booking.getOnlyCustomer(21, rooms)
-      expect(booking.getFutureBookings("2020/01/16")).to.deep.equal([
-        {
-          "id": "5fwrgu4i7k55hl6t6",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 3,
-          "roomServiceCharges": [],
-          "roomType": "single room",
-          "bidet": false,
-          "bedSize": "king",
-          "numBeds": 1,
-          "costPerNight": 491.14
+        "id": "5fwrgu4i7k55hl6t6",
+        "userID": 21,
+        "date": "2020/02/16",
+        "roomNumber": 3,
+        "roomServiceCharges": [],
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 491.14
       }
     ])
   })
 
   it('should calculate total cost spent on rooms', () => {
-    let rooms = [{
-      "number": 1,
-      "roomType": "residential suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 358.4
-  },
-  {
-      "number": 2,
-      "roomType": "suite",
-      "bidet": false,
-      "bedSize": "full",
-      "numBeds": 2,
-      "costPerNight": 477.38
-  },
-  {
-      "number": 3,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "king",
-      "numBeds": 1,
-      "costPerNight": 491.14
-  },
-  {
-      "number": 4,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 429.44
-  },
-  {
-      "number": 5,
-      "roomType": "single room",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 2,
-      "costPerNight": 340.17
-  },
-  {
-      "number": 6,
-      "roomType": "junior suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 397.02
-  }],
-
-  booking = new BookingRepo(
-    [{
-        "id": "5fwrgu4i7k55hl6sz",
-        "userID": 21,
-        "date": "2020/04/22",
-        "roomNumber": 1,
-        "roomServiceCharges": []
-    },
-    {
-        "id": "5fwrgu4i7k55hl6t5",
-        "userID": 43,
-        "date": "2020/01/24",
-        "roomNumber": 24,
-        "roomServiceCharges": []
-    },
-    {
-        "id": "5fwrgu4i7k55hl6t6",
-        "userID": 21,
-        "date": "2020/01/10",
-        "roomNumber": 2,
-        "roomServiceCharges": []
-    },
-    {
-        "id": "5fwrgu4i7k55hl6t7",
-        "userID": 20,
-        "date": "2020/02/16",
-        "roomNumber": 7,
-        "roomServiceCharges": []
-    }])
-
-    expect(booking.findTotalSpent(21, rooms)).to.equal(835.78)
+    expect(booking.findTotalSpent(21, rooms)).to.equal(849.54)
   });
 
   it('should be able to search through rooms by date', () => {
-    let rooms = [{
-      "number": 1,
-      "roomType": "residential suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 358.4
-  },
-  {
-      "number": 2,
-      "roomType": "suite",
-      "bidet": false,
-      "bedSize": "full",
-      "numBeds": 2,
-      "costPerNight": 477.38
-  },
-  {
-      "number": 3,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "king",
-      "numBeds": 1,
-      "costPerNight": 491.14
-  },
-  {
-      "number": 4,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 429.44
-  },
-  {
-      "number": 5,
-      "roomType": "single room",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 2,
-      "costPerNight": 340.17
-  },
-  {
-      "number": 6,
-      "roomType": "junior suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 397.02
-  }],
-
-    booking = new BookingRepo(
-      [{
-          "id": "5fwrgu4i7k55hl6sz",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 1,
-          "roomServiceCharges": []
-      },
+    expect(booking.findRoomsByDate("2020/02/16", rooms)).to.deep.equal([
       {
-          "id": "5fwrgu4i7k55hl6t5",
-          "userID": 43,
-          "date": "2020/02/16",
-          "roomNumber": 2,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t6",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 3,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t7",
-          "userID": 20,
-          "date": "2020/02/16",
-          "roomNumber": 4,
-          "roomServiceCharges": []
-      }])
-      expect(booking.findRoomsByDate("2020/02/16", rooms)).to.deep.equal([
-        {
-          "number": 5,
-          "roomType": "single room",
-          "bidet": true,
-          "bedSize": "queen",
-          "numBeds": 2,
-          "costPerNight": 340.17
-      },
-      {
-          "number": 6,
-          "roomType": "junior suite",
-          "bidet": true,
-          "bedSize": "queen",
-          "numBeds": 1,
-          "costPerNight": 397.02
-      }
-      ])
-  });
-
-
-
-  it('should be able to search through rooms by date', () => {
-    let rooms = [{
-      "number": 1,
-      "roomType": "residential suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 358.4
-  },
-  {
-      "number": 2,
-      "roomType": "suite",
-      "bidet": false,
-      "bedSize": "full",
-      "numBeds": 2,
-      "costPerNight": 477.38
-  },
-  {
-      "number": 3,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "king",
-      "numBeds": 1,
-      "costPerNight": 491.14
-  },
-  {
-      "number": 4,
-      "roomType": "single room",
-      "bidet": false,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 429.44
-  }],
-
-    booking = new BookingRepo(
-      [{
-          "id": "5fwrgu4i7k55hl6sz",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 1,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t5",
-          "userID": 43,
-          "date": "2020/02/16",
-          "roomNumber": 2,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t6",
-          "userID": 21,
-          "date": "2020/02/16",
-          "roomNumber": 3,
-          "roomServiceCharges": []
-      },
-      {
-          "id": "5fwrgu4i7k55hl6t7",
-          "userID": 20,
-          "date": "2020/02/16",
-          "roomNumber": 4,
-          "roomServiceCharges": []
-      }])
-
-      expect(booking.findRoomsByDate("2020/02/16", rooms)).to.equal("So sorry, there are no rooms available for this date! Please choose a different date.")
-    })
-
-    it('should be able to filter through available rooms by guest preferences', () => {
-      let rooms = [{
         "number": 1,
         "roomType": "residential suite",
         "bidet": true,
         "bedSize": "queen",
         "numBeds": 1,
         "costPerNight": 358.4
-    },
-    {
-        "number": 2,
-        "roomType": "suite",
-        "bidet": false,
-        "bedSize": "full",
+      },  
+      {
+        "number": 5,
+        "roomType": "single room",
+        "bidet": true,
+        "bedSize": "queen",
         "numBeds": 2,
-        "costPerNight": 477.38
-    },
-    {
-        "number": 3,
-        "roomType": "single room",
-        "bidet": false,
-        "bedSize": "king",
-        "numBeds": 1,
-        "costPerNight": 491.14
-    },
-    {
-        "number": 4,
-        "roomType": "single room",
-        "bidet": false,
+        "costPerNight": 340.17
+      },
+      {
+        "number": 6,
+        "roomType": "junior suite",
+        "bidet": true,
         "bedSize": "queen",
         "numBeds": 1,
-        "costPerNight": 429.44
-    },
-    {
-      "number": 5,
-      "roomType": "single room",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 2,
-      "costPerNight": 340.17
-  },
-  {
-      "number": 6,
-      "roomType": "junior suite",
-      "bidet": true,
-      "bedSize": "queen",
-      "numBeds": 1,
-      "costPerNight": 397.02
-  }],
-  
-      bookings = new BookingRepo(
-        [{
-            "id": "5fwrgu4i7k55hl6sz",
-            "userID": 21,
-            "date": "2020/02/16",
-            "roomNumber": 1,
-            "roomServiceCharges": []
-        },
-        {
-            "id": "5fwrgu4i7k55hl6t5",
-            "userID": 43,
-            "date": "2020/02/16",
-            "roomNumber": 2,
-            "roomServiceCharges": []
-        },
-        {
-            "id": "5fwrgu4i7k55hl6t6",
-            "userID": 21,
-            "date": "2020/02/16",
-            "roomNumber": 3,
-            "roomServiceCharges": []
-        },
-        {
-            "id": "5fwrgu4i7k55hl6t7",
-            "userID": 20,
-            "date": "2020/02/16",
-            "roomNumber": 4,
-            "roomServiceCharges": []
-        }])
+        "costPerNight": 397.02
+      }
+    ])
+  });
 
-      bookings.findRoomsByDate("2020/02/16", rooms)
-
-      expect(bookings.filterByPreferences(["queen", true, 2])).to.deep.equal(
-        [{
-          "number": 5,
-          "roomType": "single room",
-          "bidet": true,
-          "bedSize": "queen",
-          "numBeds": 2,
-          "costPerNight": 340.17
-      }])
-    })
-
+  it('should be able to filter through available rooms by guest preferences', () => {
+    booking.findRoomsByDate("2020/02/16", rooms)
+    expect(booking.filterByPreferences(["queen", true, 2])).to.deep.equal(
+      [{
+        "number": 5,
+        "roomType": "single room",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 2,
+        "costPerNight": 340.17
+    }])
+  })
 });
